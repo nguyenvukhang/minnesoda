63c46d1673565e2cbc9dac869dd96a2f4ed92b9d
nguyenvukhang <brew4k@gmail.com>
2022-01-20 23:01:36 +0800

    added menubar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { MDXProvider } from '@mdx-js/react'
 import '@/src/global.css'
+import Link from 'next/link'
 
 const tailwind = {
   h1: 'text-gray-900',
@@ -13,10 +14,37 @@ Object.entries(tailwind).forEach((e) => {
   components[Tag] = ({ children }) => <Tag className={e[1]}>{children}</Tag>
 })
 
+components.a = ({ href, children }) => {
+  const style = 'text-blue-500'
+  return (
+    <Link href={href}>
+      <a className={style}>{children}</a>
+    </Link>
+  )
+}
+
+function toggleMathjax() {
+  alert('hello there')
+}
+
+const Menubar = () => {
+  return (
+    <div className="mb-8 flex flex-row">
+      <div className="flex-1">
+        <components.a href="/">Home</components.a>
+      </div>
+      <div className="flex">
+        <a className='text-blue-500 cursor-pointer' onClick={toggleMathjax}>MathJax</a>
+      </div>
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
   return (
     <MDXProvider components={components}>
-      <div className='my-10 mx-auto max-w-3xl px-12'>
+      <div className="my-8 mx-auto max-w-3xl px-12">
+        <Menubar />
         <Component {...pageProps} />
       </div>
     </MDXProvider>
