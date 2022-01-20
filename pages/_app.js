1d127d83f544cced6d12583d8c039c094c0cd245
nguyenvukhang <brew4k@gmail.com>
2022-01-20 23:38:19 +0800

    made mathjax togglable

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import { MDXProvider } from '@mdx-js/react'
 import '@/src/global.css'
 import Link from 'next/link'
+import { MathJax, MathJaxContext } from 'better-react-mathjax'
+import { useState } from 'react'
 
 const tailwind = {
   h1: 'text-gray-900',
@@ -15,7 +17,7 @@ Object.entries(tailwind).forEach((e) => {
 })
 
 components.a = ({ href, children }) => {
-  const style = 'text-blue-500'
+  const style = 'text-blue-500 hover:underline'
   return (
     <Link href={href}>
       <a className={style}>{children}</a>
@@ -23,30 +25,56 @@ components.a = ({ href, children }) => {
   )
 }
 
-function toggleMathjax() {
-  alert('hello there')
-}
+const Menubar = ({ functions }) => {
+  const [mathtype, setMathtype] = functions
 
-const Menubar = () => {
+  function toggleMathjax() {
+    setMathtype(!mathtype)
+  }
+
+  const toggleButtonColor = mathtype ? 'text-blue-500' : 'text-neutral-400'
   return (
     <div className="mb-8 flex flex-row">
       <div className="flex-1">
         <components.a href="/">Home</components.a>
       </div>
       <div className="flex">
-        <a className='text-blue-500 cursor-pointer' onClick={toggleMathjax}>MathJax</a>
+        <a
+          className={`${toggleButtonColor} cursor-pointer hover:underline`}
+          onClick={() => toggleMathjax()}
+        >
+          Math
+        </a>
       </div>
     </div>
   )
 }
 
 function MyApp({ Component, pageProps }) {
+  const [mathtype, setMathtype] = useState(true)
+  components.pre = ({ children }) => {
+    return mathtype ? (
+      <MathJax>{`$$${children.props.children}$$`}</MathJax>
+    ) : (
+      <div className="my-4 text-pink-500">{children}</div>
+    )
+  }
+  components.inlineCode = ({ children }) => {
+    return mathtype ? (
+      <MathJax inline>{`\\(${children}\\)`}</MathJax>
+    ) : (
+      <div className="text-pink-500">{children}</div>
+    )
+  }
+
   return (
     <MDXProvider components={components}>
-      <div className="my-8 mx-auto max-w-3xl px-12">
-        <Menubar />
-        <Component {...pageProps} />
-      </div>
+      <MathJaxContext>
+        <div className="my-8 mx-auto max-w-3xl px-12">
+          <Menubar functions={[mathtype, setMathtype]} />
+          <Component {...pageProps} />
+        </div>
+      </MathJaxContext>
     </MDXProvider>
   )
 }
