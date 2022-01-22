68f5efee84247eebaaee5b47b92ca93f39b263ee
nguyenvukhang <brew4k@gmail.com>
2022-01-22 23:08:05 +0800

    smoothened tooltip a bit

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import { MDXProvider } from '@mdx-js/react'
 import '../src/global.css'
 import { MathJaxContext } from 'better-react-mathjax'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Menubar from '../src/Menubar'
 import getComponents from '../src/MdxComponents'
-import PermaFloat, { handleMouseUp } from '../src/PermaFloat'
+import { handleMouseUp, removeFloat } from '../src/PermaFloat'
 import MathjaxConfig from '../src/MathjaxConfig'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   const [math, setMath] = useState(true)
-  const [tooltip, setTooltip] = useState(false)
   const components = getComponents({ math })
 
   return (
@@ -17,12 +18,12 @@ function MyApp({ Component, pageProps }) {
       <MathJaxContext config={MathjaxConfig}>
         <div
           className="my-8 mx-auto max-w-3xl px-6"
-          onMouseUp={handleMouseUp}
+          onMouseUp={() => handleMouseUp({ router })}
+          onMouseDown={removeFloat}
         >
           <Menubar state={[math, setMath]} components={components} />
           <Component {...pageProps} />
         </div>
-        <PermaFloat state={[tooltip, setTooltip]}/>
       </MathJaxContext>
     </MDXProvider>
   )
--- src/PermaFloat.js
+++ src/PermaFloat.js
@@ -1,119 +1,41 @@
-import Link from 'next/link'
-import { useEffect, useRef } from 'react'
 import references from '../pages/references.json'
+import ReactDOM from 'react-dom'
 
-// function handleMouseUp(setFloatRect) {
-//   const selection = window.getSelection()
-//   const query = selection.toString().toLowerCase().trim()
-//
-//   if (query === '') {
-//     return
-//   }
-//
-//   if (references.hasOwnProperty(query)) {
-//     // console.log('has reference!')
-//     const range = selection.getRangeAt(0)
-//     const rect = range.getBoundingClientRect()
-//
-//     const div = document.createElement('div')
-//     div.style.border = '2px solid black' // with outline
-//     div.style.position = 'fixed' // fixed positioning = easy mode
-//     div.style.top = rect.top + 'px' // set coordinates
-//     div.id = 'perma-float'
-//     div.style.left = rect.left + 'px'
-//     div.style.height = rect.height + 'px' // and size
-//     div.style.width = rect.width + 'px'
-//     document.body.appendChild(div)
-//
-//     // setFloatRect({
-//     //   top: rect.y + rect.height + 4,
-//     //   left: rect.x,
-//     //   display: 'block',
-//     //   position: 'absolute',
-//     //   boxShadow: '2px 2px 2px #AAAAAA',
-//     //   query,
-//     // })
-//   } else {
-//     // console.log("doesn't have a reference")
-//   }
-//   // console.log('mouseup', query)
-// }
-
-function checkOutsideClick({ ref, floatRect, closeFloat }) {
-  useEffect(() => {
-    const existingFloat = document.getElementById('perma-float')
-    function handleClickOutside(event) {
-      if (floatRect.display === 'none') {
-        return
-      }
-      if (ref.current && !ref.current.contains(event.target)) {
-        closeFloat()
-      }
-    }
-
-    // Bind the event listener
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      // Unbind the event listener on clean up
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [ref, floatRect])
+const Tooltip = ({ router, query, removeFloat }) => {
+  console.log(references[query])
+  const href = references[query].definition
+  function handleClick() {
+    removeFloat()
+    router.push(href)
+  }
+  return (
+    <div className="absolute flex flex-col bg-blue-100 px-3 py-2">
+      <a onClick={handleClick}>Go to definition</a>
+      <div className="mt-1-h">Go to reference</div>
+      <div className="flex flex-col">
+        {references[query].references.map((e) => (
+          <Link href={e[1]}>
+            <a onClick={() => this.remove()}>{e[0]}</a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-// const PermaFloat = ({ floatRect, setFloatRect }) => {
-//   const floatRef = useRef(null)
-//   function closeFloat() {
-//     setFloatRect({
-//       height: 0,
-//       width: 0,
-//       display: 'none',
-//     })
-//   }
-//   checkOutsideClick({ ref: floatRef, floatRect, closeFloat })
-//   const isOpen = floatRect.display === 'block'
-//   const query = floatRect.query
-//   // console.log(references[query]?.references)
-//   return isOpen ? (
-//     <div
-//       ref={floatRef}
-//       className="flex flex-col bg-blue-100 px-3 py-2"
-//       style={floatRect}
-//     >
-//       <Link href={references[query].definition}>
-//         <a onClick={() => closeFloat()}>Go to definition</a>
-//       </Link>
-//       <div className="mt-1-h">Go to reference</div>
-//       <div className="flex flex-col">
-//         {references[query].references.map((e) => (
-//           <Link href={e[1]}>
-//             <a onClick={() => closeFloat()}>{e[0]}</a>
-//           </Link>
-//         ))}
-//       </div>
-//     </div>
-//   ) : null
-// }
-
-const PermaFloat = ({ state }) => {
-  const [tooltip, setTooltip] = state
-
-  return null
+function removeFloat() {
+  const exisitngFloat = document.getElementById('reference-tooltip')
+  if (exisitngFloat) {
+    exisitngFloat.remove()
+  }
 }
 
-function handleMouseUp() {
+function handleMouseUp({ router }) {
+  removeFloat()
   const selection = window.getSelection()
   const query = selection.toString().toLowerCase().trim()
 
-  function removeFloat() {
-    const exisitngFloat = document.getElementById('perma-float')
-    if (exisitngFloat) {
-      console.log('reset tooltip')
-      exisitngFloat.remove()
-    }
-  }
-
   if (query === '') {
-    removeFloat()
     return
   }
 
@@ -121,20 +43,14 @@ function handleMouseUp() {
     // console.log('has reference!')
     const range = selection.getRangeAt(0)
     const rect = range.getBoundingClientRect()
-
-    const div = document.createElement('div')
-    div.style.border = '2px solid black' // with outline
-    div.style.position = 'fixed' // fixed positioning = easy mode
-    div.style.top = rect.top + 'px' // set coordinates
-    div.id = 'perma-float'
-    div.style.left = rect.left + 'px'
-    div.style.height = rect.height + 'px' // and size
-    div.style.width = rect.width + 'px'
-    document.body.appendChild(div)
-  } else {
-    removeFloat()
+    const i = document.createElement('div')
+    i.style.position = 'absolute' // fixed positioning = easy mode
+    i.style.top = rect.top + 'px' // set coordinates
+    i.style.left = rect.left + 'px'
+    i.id = 'reference-tooltip'
+    document.body.appendChild(i)
+    ReactDOM.render(Tooltip({ router, query, removeFloat }), i)
   }
 }
 
-export default PermaFloat
-export { handleMouseUp }
+export { handleMouseUp, removeFloat }
