cddef541128bd8c7d0609bcc611101568da90f79
nguyenvukhang <brew4k@gmail.com>
2022-01-22 16:16:02 +0800

    custom 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+export default function Custom404() {
+  return (
+    <div className="flex justify-center h-screen-80">
+      <div className="flex flex-col justify-center">
+        <div className='mx-auto'>404</div>
+        <div className='mx-auto'>@everyone: page not found</div>
+        <div className='mx-auto text-gray-300'>@khang: pick yourself up and get the f*ck back on track.</div>
+      </div>
+    </div>
+  )
+}
