0725c6d03b6d4faae648336e74046aede7dde5bd
nguyenvukhang <brew4k@gmail.com>
2022-01-23 15:54:07 +0800

    content edits

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function Footer() {
     <div className="h-48 flex justify-center">
       <div className="flex flex-col">
         <div className="flex-1" />
-        <div className="text-gray-400 mb-10">khang</div>
+        <div className="text-gray-300 mb-10">@khang</div>
       </div>
     </div>
   )
