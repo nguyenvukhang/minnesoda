c09251dcf96969563ce52729259284c4097c1426
nguyenvukhang <brew4k@gmail.com>
2022-01-23 22:34:26 +0800

    updated menubar

--- src/Menubar.jsx
+++ src/Menubar.jsx
@@ -1,25 +1,39 @@
 const Menubar = ({ state, components }) => {
   const [math, setMath] = state
-  const toggleButtonColor = math ? 'text-accent' : 'text-neutral-400'
+  const toggleButtonColor = math ? 'text-pink-500' : 'text-neutral-400'
   const env = process.env.NODE_ENV
+
+  const Reload = () => {
+    return env === 'development' ? (
+      <>
+        <components.a href="#">Reload</components.a>
+        <div className="w-12" />
+      </>
+    ) : null
+  }
+
+  const MathToggle = () => {
+    return (
+      <a
+        className="text-accent cursor-pointer hover:underline"
+        onClick={() => setMath(!math)}
+      >
+        Math:
+        <span className={`${toggleButtonColor} cursor-pointer hover:underline`}>
+          {math ? ' on' : ' off'}
+        </span>
+      </a>
+    )
+  }
+
   return (
     <div className="mb-8 flex flex-row">
       <div className="flex-1">
         <components.a href="/">Home</components.a>{' '}
       </div>
       <div className="flex">
-        {env === 'development' ? (
-          <>
-            <components.a href="#">Reload</components.a>
-            <div className="w-12" />
-          </>
-        ) : null}
-        <a
-          className={`${toggleButtonColor} cursor-pointer hover:underline`}
-          onClick={() => setMath(!math)}
-        >
-          Math
-        </a>
+        <Reload />
+        <MathToggle />
       </div>
     </div>
   )
