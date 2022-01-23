21ff3202e1b8595f536d390da8a3a36434770651
nguyenvukhang <brew4k@gmail.com>
2022-01-23 12:06:24 +0800

    added arrow to tooltip

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -4,24 +4,37 @@ import Link from 'next/link'
 
 const Tooltip = ({ router, query, removeTooltip, transform }) => {
   // console.log(references[query])
-  console.log("transform", transform)
+  console.log('transform', transform)
   const href = references[query].definition
   function handleClick() {
     removeTooltip()
     router.push(href)
   }
-  const style = transform === 'up' ? {
-    transform: "translateY(-100%)"
-  } : null
+  const style =
+    transform === 'above'
+      ? {
+          transform: 'translateY(calc(-100% - 8px))',
+        }
+      : null
+  const Arrow = () => {
+    const down =
+      'w-0 h-0 border-x-8 border-t-8 border-x-transparent border-t-blue-100'
+    const rotate = transform === 'above' ? 'translate-y-[-8px]' : 'rotate-180'
+    return <div className={`${down} ${rotate}`} />
+  }
   return (
-    <div className="absolute flex flex-col bg-blue-100 px-3 py-2" style={style}>
-      <a onClick={handleClick}>Go to definition</a>
-      <div className="mt-1-h">Go to reference</div>
-      <div className="flex flex-col">
-        {references[query].references.map((e) => (
-          <a onClick={handleClick}>{e[0]}</a>
-        ))}
+    <div>
+      {transform === 'below' ? <Arrow /> : null}
+      <div className="absolute flex flex-col bg-blue-100 px-3 py-2" style={style}>
+        <a onClick={handleClick}>Go to definition</a>
+        <div className="mt-1-h">Go to reference</div>
+        <div className="flex flex-col">
+          {references[query].references.map((e) => (
+            <a onClick={handleClick}>{e[0]}</a>
+          ))}
+        </div>
       </div>
+      {transform === 'above' ? <Arrow /> : null}
     </div>
   )
 }
@@ -53,11 +66,11 @@ function handleMouseUp({ router }) {
     var transform
     // check if element is more than halfway down the screen
     if (rect.top > window.innerHeight / 2) {
-      i.style.top =  window.scrollY +rect.top + 'px' // set coordinates
-      transform = 'up'
+      i.style.top = window.scrollY + rect.top + 'px' // set coordinates
+      transform = 'above'
     } else {
-      i.style.top =  window.scrollY +rect.top + rect.height + 'px' // set coordinates
-      transform = 'normal'
+      i.style.top = window.scrollY + rect.top + rect.height + 'px' // set coordinates
+      transform = 'below'
     }
     document.body.appendChild(i)
     ReactDOM.render(Tooltip({ router, query, removeTooltip, transform }), i)
