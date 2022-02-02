593234be2bf5151a170ac2041fde7f7391f5037f
nguyenvukhang <brew4k@gmail.com>
2022-02-02 18:42:03 +0800

    update 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ export default function Custom404() {
     <div className="flex justify-center h-screen-80">
       <div className="flex flex-col justify-center">
         <div className="mx-auto">404</div>
-        <div className="mx-auto">@everyone: page not found</div>
+        <div className="mx-auto">page not found</div>
         <a className="mx-auto mt-1-h" href="/">
           back home
         </a>
