7ba02135b347f8b14006f00ce36d33fc95a7dc48
nguyenvukhang <brew4k@gmail.com>
2022-04-17 17:35:18 +0800

    added change of base

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,10 @@
 const Container = ({ children }) => {
   const centering = 'mx-auto px-6'
   const sizing = 'max-w-3xl'
+  const margin = 'mb-72'
   const debug = false
   const d = debug ? 'bg-blue-100' : ''
-  return <div className={`${centering} ${sizing} ${d}`}>{children}</div>
+  return <div className={`${centering} ${sizing} ${margin} ${d}`}>{children}</div>
 }
 
 export default Container
