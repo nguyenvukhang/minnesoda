60bcd0e772e60654d60c6c3c892b897e0046f2f8
nguyenvukhang <brew4k@gmail.com>
2022-04-20 23:42:59 +0800

    adjusted css

--- src/components/mdx/Table.jsx
+++ src/components/mdx/Table.jsx
@@ -17,19 +17,50 @@ const tr = ({ children }) => <tr>{children}</tr>
 
 /* CELLS */
 
+const hasOwn = (obj, key) => {
+  if (obj === null || obj === undefined) {
+    return false
+  }
+  return Object.keys(obj).includes(key)
+}
+
+const getMathStatus = (obj) => {
+  if (hasOwn(obj, 'props')) {
+    if (hasOwn(obj.props, 'className')) {
+      return obj.props.className.includes('math')
+    }
+  }
+  return false
+}
+
 // table header element
-const th = ({ children }) => (
-  <th>
-    <div className="text-center">{children}</div>
-  </th>
-)
+const th = ({ children }) => {
+  const hasMath = getMathStatus(children)
+  return hasMath ? (
+    <th className="py-4">
+      <div className="text-center">{children}</div>
+    </th>
+  ) : (
+    <th>
+      <div className="text-center">{children}</div>
+    </th>
+  )
+}
 
 // table element
-const td = ({ children }) => (
-  <td>
-    <div className="text-center">{children}</div>
-  </td>
-)
+const td = ({ children }) => {
+  const hasMath = getMathStatus(children)
+  console.log('element', hasMath)
+  return hasMath ? (
+    <td className="py-4">
+      <div className="text-center">{children}</div>
+    </td>
+  ) : (
+    <td>
+      <div className="text-center">{children}</div>
+    </td>
+  )
+}
 
 export { thead, tbody, td, tr, th }
 export default Table
