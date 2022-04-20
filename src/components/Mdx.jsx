f09d54dbc9f4e9693d0c02da7d922503525418c4
nguyenvukhang <brew4k@gmail.com>
2022-04-20 22:55:06 +0800

    adjusted css for table

--- src/components/Mdx.jsx
+++ src/components/Mdx.jsx
@@ -1,21 +1,19 @@
 import a from './mdx/Link'
-import table from './mdx/Table'
+import table, { thead, tbody, tr, td, th } from './mdx/Table'
 import headers from './mdx/Headers'
 
+// with table
+// const Mdx = { a, table, thead, tbody, tr, td, th, ...headers }
+// without table
 const Mdx = { a, table, ...headers }
 
 const Space = () => {
-  return (
-    <div className='h-28'/>
-  )
+  return <div className="h-28" />
 }
 
 const BigSpace = () => {
-  return (
-    <div className='h-96'/>
-  )
+  return <div className="h-96" />
 }
-
 
 export default Mdx
 export { Space, BigSpace }
--- src/components/mdx/Table.jsx
+++ src/components/mdx/Table.jsx
@@ -1,7 +1,34 @@
 const Table = ({ children }) => (
-  <div className='tw-table'>
-    <table>{children}</table>
-  </div>
+  <table className="border-collapse table-auto tw-table">{children}</table>
 )
 
+const padding = 'p-2'
+const border = 'border border-gray-600'
+
+/* CONTAINERS */
+
+// table body wrapper
+const tbody = ({ children }) => (
+  <tbody className={`bg-green-100 ${padding} ${border}`}>{children}</tbody>
+)
+
+// table header wrapper
+const thead = ({ children }) => (
+  <thead className={`bg-green-100 ${padding} ${border}`}>{children}</thead>
+)
+
+// table row
+const tr = ({ children }) => <tr>{children}</tr>
+
+// table header element
+const th = ({ children }) => (
+  <th className={`bg-green-100 ${padding} ${border}`}>{children}</th>
+)
+
+// table element
+const td = ({ children }) => (
+  <td className={`bg-yellow-100 ${padding} ${border}`}>{children}</td>
+)
+
+export { thead, tbody, td, tr, th }
 export default Table
